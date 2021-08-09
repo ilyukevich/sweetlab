afd96f9f5204e7c5f62e0bc078af27f1998922a2
ilyukevich <9009901@gmail.com>
2021-08-09 21:32:33 +0300

    models, urls, config

--- config/settings.py
+++ config/settings.py
@@ -23,6 +23,13 @@ DEBUG = True
 # all hosts
 ALLOWED_HOSTS = ['*']
 
+# ALLOWED_HOSTS = [
+#         "127.0.0.1",
+#         "localhost",
+#         "[::1]",
+#         "testserver",
+# ]
+
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -31,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'apps.core.apps.ShopConfig',
+    'apps.shop.apps.ShopConfig',
 ]
 
 MIDDLEWARE = [
